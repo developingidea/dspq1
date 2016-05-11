719e56fdfde0657e4724cf56db3c7b7a806cef85
developingidea <zalai.gabor@artinvent.hu>
2016-05-11 11:59:06 +0200

    fixed

--- js/ui.js
+++ js/ui.js
@@ -1,1 +1,123 @@
-var _0xefc1=["\x66\x69\x72\x73\x74","\x5F\x6D\x61\x78\x57\x69\x64\x74\x68","","\x77\x69\x6E\x64\x6F\x77\x73\x62\x6F\x78","\x66\x6E","\x69\x64","\x61\x74\x74\x72","\x2E\x6D\x6F\x76\x65\x20\x75\x6C","\x66\x69\x6E\x64","\x68\x65\x69\x67\x68\x74","\x75\x6C\x23","\x20\x6C\x69\x3A\x6E\x74\x68\x2D\x63\x68\x69\x6C\x64\x28","\x29","\x61\x6E\x69\x6D\x61\x74\x69\x6F\x6E\x53\x70\x65\x65\x64","\x61\x6E\x69\x6D\x61\x74\x65","\x70\x61\x72\x65\x6E\x74","\x23","\x63\x6C\x69\x63\x6B","\x2E\x73\x69\x6D\x70\x6C\x65\x73\x6C\x69\x64\x65\x72\x2D\x63\x6F\x6E\x74\x61\x69\x6E\x65\x72\x20\x6C\x69","\x64\x61\x74\x61\x2D\x6E\x61\x76","\x61\x63\x74\x69\x76\x65","\x72\x65\x6D\x6F\x76\x65\x43\x6C\x61\x73\x73","\x61\x64\x64\x43\x6C\x61\x73\x73","\x6F\x6E","\x69\x6E\x69\x74","\x64\x65\x6C\x61\x79","\x63\x6F\x75\x6E\x74","\x64\x61\x74\x61","\x72\x6F\x75\x6E\x64","\x74\x65\x78\x74","\x23\x77\x62\x2D\x74\x6F\x70\x2D\x6D\x65\x73\x73\x61\x67\x65","\x23\x77\x62\x2D\x74\x6F\x70\x2D\x6E\x6F\x74\x69\x66","\x72\x65\x61\x64\x79","\x23\x77\x62\x2D\x74\x6F\x70\x2D\x73\x65\x61\x72\x63\x68","\x23\x77\x62\x2D\x74\x6F\x70\x2D\x73\x65\x61\x72\x63\x68\x2D\x63\x6F\x6E\x74\x61\x69\x6E\x65\x72","\x23\x77\x62\x2D\x74\x6F\x70\x2D\x73\x65\x61\x72\x63\x68\x2D\x73\x65\x6E\x64","\x57\x69\x6E\x64\x6F\x77\x73\x42\x6F\x78\x55\x49","\x77\x69\x64\x74\x68","\x23\x77\x62\x2D\x74\x6F\x70\x2D\x6C\x6F\x67\x6F","\x63\x73\x73","\x73\x65\x61\x72\x63\x68\x52\x65\x73\x69\x7A\x65","\x72\x65\x73\x69\x7A\x65","\x77\x68\x69\x63\x68","\x74\x72\x69\x67\x67\x65\x72","\x6B\x65\x79\x70\x72\x65\x73\x73","\x76\x61\x6C","\x4B\x65\x72\x65\x73\xE9\x73\x20\x61\x20\x6B\xF6\x76\x65\x74\x6B\x65\x7A\u0151\x72\x65\x3A\x20\x3C\x73\x70\x61\x6E\x3E\x22","\x22\x3C\x73\x70\x61\x6E\x3E","\x68\x74\x6D\x6C","\x23\x77\x62\x2D\x73\x65\x61\x72\x63\x68\x2D\x74\x61\x72\x67\x65\x74\x20\x6C\x69","\x73\x6C\x69\x64\x65\x54\x6F\x67\x67\x6C\x65","\x63\x6C\x65\x61\x72\x51\x75\x65\x75\x65","\x68\x69\x64\x65","\x23\x77\x62\x2D\x73\x65\x61\x72\x63\x68\x2D\x74\x61\x72\x67\x65\x74","\x41\x20\x6B\x65\x72\x65\x73\xE9\x73\x69\x20\x6D\x65\x7A\u0151\x20\xFC\x72\x65\x73\x21","\x66\x6F\x63\x75\x73"];window[_0xefc1[0]]=true;window[_0xefc1[1]]=_0xefc1[2];$[_0xefc1[4]][_0xefc1[3]]=function(){$logic={animationSpeed:300,_updateHeight:function(_0xb61ax1,_0xb61ax2){id=_0xb61ax1[_0xefc1[8]](_0xefc1[7])[_0xefc1[6]](_0xefc1[5]);_Height=$(_0xefc1[10]+id+_0xefc1[11]+_0xb61ax2+_0xefc1[12])[_0xefc1[9]]();$(_0xefc1[16]+id+_0xefc1[2])[_0xefc1[15]]()[_0xefc1[14]]({height:_Height},$logic[_0xefc1[13]])},_navigationButtons:function(){$(document)[_0xefc1[23]](_0xefc1[17],_0xefc1[18],function(){nth=$(this)[_0xefc1[6]](_0xefc1[19]);$clickConatiner=$(this)[_0xefc1[15]]()[_0xefc1[15]]();$logic._updateHeight($clickConatiner,eval(nth)+1);$clickConatiner[_0xefc1[8]](_0xefc1[18])[_0xefc1[21]](_0xefc1[20]);$(this)[_0xefc1[22]](_0xefc1[20]);$clickConatiner[_0xefc1[8]](_0xefc1[7])[_0xefc1[14]]({marginLeft:-nth*_maxWidth},$logic[_0xefc1[13]])})},init:function(){if(window[_0xefc1[0]]){window[_0xefc1[0]]=false;$logic._navigationButtons()}}};$logic[_0xefc1[24]]()};function countUp(_0xb61ax4,_0xb61ax5){setTimeout(function(){$(_0xb61ax4)[_0xefc1[25]](300)[_0xefc1[14]]({marginTop:0,opacity:1},1000);setTimeout(function(){var _0xb61ax6=$(_0xb61ax4)[_0xefc1[27]](_0xefc1[26]);if(_0xb61ax6==0||_0xb61ax6==undefined){return};var _0xb61ax7=Math[_0xefc1[28]](_0xb61ax6),_0xb61ax8=$(_0xb61ax4),_0xb61ax9=1,_0xb61axa=100+_0xb61ax9*40;var _0xb61axb=setInterval(function(){if(_0xb61ax9-1<_0xb61ax6){_0xb61ax8[_0xefc1[29]](_0xb61ax9);_0xb61ax9++}else {clearInterval(_0xb61axb)}},_0xb61axa)},700)},_0xb61ax5)}$(document)[_0xefc1[32]](function(){countUp(_0xefc1[30]);countUp(_0xefc1[31],200)});var $document=$(document),$window=$(window),$search=$(_0xefc1[33]),$searchContainer=$(_0xefc1[34]),$searchSend=$(_0xefc1[35]);window[_0xefc1[36]]={searchResize:function(){width=$window[_0xefc1[37]]()-($(_0xefc1[38])[_0xefc1[37]]()+5+350);$searchContainer[_0xefc1[39]]({width:width})}};$(window)[_0xefc1[41]](function(){WindowsBoxUI[_0xefc1[40]]()});$(function(){WindowsBoxUI[_0xefc1[40]]();setTimeout(function(){WindowsBoxUI[_0xefc1[40]]()},100)});$search[_0xefc1[44]](function(_0xb61ax11){if(_0xb61ax11[_0xefc1[42]]==13){$searchSend[_0xefc1[43]](_0xefc1[17])}});$(_0xefc1[35])[_0xefc1[17]](function(){if($search[_0xefc1[45]]()){$(_0xefc1[49])[_0xefc1[48]](_0xefc1[46]+$search[_0xefc1[45]]()+_0xefc1[47]);$(_0xefc1[53])[_0xefc1[52]]()[_0xefc1[51]]()[_0xefc1[50]](500)[_0xefc1[25]](2000)[_0xefc1[50]](500)}else {$(_0xefc1[49])[_0xefc1[48]](_0xefc1[54]);$(_0xefc1[53])[_0xefc1[52]]()[_0xefc1[51]]()[_0xefc1[50]](500)[_0xefc1[25]](2000)[_0xefc1[50]](500);$search[_0xefc1[55]]()}})
+window.first = true;
+window._maxWidth = '';
+$.fn.windowsbox = function(){	
+	$logic = {
+		animationSpeed: 300,
+		_updateHeight: function(current, nth) {			
+			id = current.find('.move ul').attr('id')
+			_Height = $('ul#'+id+' li:nth-child('+nth+')').height();
+			$('#'+id+'').parent().animate({ height: _Height }, $logic.animationSpeed)			
+		},
+		_navigationButtons: function() {
+			$(document).on('click', '.simpleslider-container li', function (){				
+				nth = $(this).attr('data-nav')
+				$clickConatiner = $(this).parent().parent()
+				$logic._updateHeight($clickConatiner, eval(nth)+1)
+				$clickConatiner.find('.simpleslider-container li').removeClass('active')
+				$(this).addClass('active')
+				$clickConatiner.find('.move ul').animate({ marginLeft: -nth*_maxWidth }, $logic.animationSpeed)
+			});		
+		},
+		init: function() {
+			if ( window.first ) {
+				window.first = false;
+				$logic._navigationButtons()
+			}
+		}
+	};
+	$logic.init();
+};
+
+// $.fn.countUp = function(){	
+// 	$logic = {
+// 		int_speed: 50,
+// 		run: function() {
+// 		      var count = $('body').find('data-count').data('count');
+// 		      var speed = Math.round(count),
+// 		        $display = $(this),
+// 		        run_count = 1,
+// 		        int_speed = 40 - count * 0.3; // speedup :)
+// 		      var int = setInterval(function() {
+// 		        if (run_count - 1 < count) {
+// 		          $display.text(run_count);
+// 		          run_count++;
+// 		        } else {
+// 		          clearInterval(int);
+// 		        }
+// 		      }, int_speed);
+// 		}
+// 	};
+// 	$logic.run();
+// };
+
+ function countUp(countclass, startdelay) { 
+ 	setTimeout(function() { 
+	 	$(countclass).delay(300).animate({
+	        marginTop: 0, opacity: 1
+	    }, 1000);
+
+	    setTimeout(function() {   
+	      var count = $(countclass).data('count');
+	      if ( count == 0 || count == undefined) return
+	      var speed = Math.round(count),
+	        $display = $(countclass),
+	        run_count = 1,
+	        int_speed = 100+run_count*40; // speedup :)
+	      var int = setInterval(function() {
+	        if (run_count - 1 < count) {
+	          $display.text(run_count);
+	          run_count++;
+	        } else {
+	          clearInterval(int);
+	        }
+	      }, int_speed);
+	    }, 700);
+	 }, startdelay);
+  }
+  $(document).ready(function() {
+    countUp('#wb-top-message');
+    countUp('#wb-top-notif', 200);
+  });
+
+
+
+
+
+var $document = $(document),
+	$window = $(window),
+	$search = $('#wb-top-search'),
+	$searchContainer = $('#wb-top-search-container'),
+	$searchSend = $('#wb-top-search-send');
+
+window.WindowsBoxUI = {
+	searchResize: function(){
+		width = $window.width()-($('#wb-top-logo').width()+5+350);
+		$searchContainer.css({ width: width })
+	}
+}
+
+$(window).resize(function(){
+	WindowsBoxUI.searchResize();
+});
+$(function(){
+	WindowsBoxUI.searchResize();
+	setTimeout(function() {
+		WindowsBoxUI.searchResize();
+	}, 100);	
+});
+
+$search.keypress(function (event) {
+    if (event.which == 13) {
+        $searchSend.trigger('click')
+    }
+});
+
+$('#wb-top-search-send').click(function() {
+	if( $search.val() ) {
+		 $('#wb-search-target li').html('Keresés a következőre: <span>"'+$search.val()+'"<span>'); $('#wb-search-target').hide().clearQueue().slideToggle(500).delay(2000).slideToggle(500);
+	}
+	else {
+		$('#wb-search-target li').html('A keresési mező üres!'); $('#wb-search-target').hide().clearQueue().slideToggle(500).delay(2000).slideToggle(500); $search.focus()
+	}
+	
+});
